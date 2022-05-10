e061bec01f4d57cd42512cc5f0b4d2f8a59b8705
annanharry <annanharry@students.uonbi.ac.ke>
2022-05-10 20:46:11 +0300

    2nd try

--- code/admin/addmember.php
+++ code/admin/addmember.php
@@ -7,8 +7,8 @@ $surname = $_POST['surname'];
 $othernames = $_POST['othernames'];
 $employment = $_POST['employment'];
 
-$sql = mysqli_query($conn, "INSERT INTO members(first_name, surname, other_names, Employment)
-    VALUES ('$firstname','$surname','$othernames','$employment')");
+$sql = mysqli_query($conn, "INSERT INTO members(member_id_code, first_name, surname, other_names, Employment)
+    VALUES ('M','$firstname','$surname','$othernames','$employment')");
 
 header("location:managememberadd.html");
 
