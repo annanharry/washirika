58c81ec4e943080e6847d8d5593d92f7480c3e0b
annanharry <annanharry@students.uonbi.ac.ke>
2022-05-10 19:56:28 +0300

    add member php

--- code/admin/addmember.php
+++ code/admin/addmember.php
@@ -2,4 +2,14 @@
 
 include "../ses/connect.php";
 
+$firstname = $_POST['firstname'];
+$surname = $_POST['surname'];
+$othernames = $_POST['othernames'];
+$employment = $_POST['employment'];
+
+$sql = mysqli_query($conn, "INSERT INTO members(first_name, surname, other_names, Employment)
+    VALUES ('$firstname','$surname','$othernames','$employment')");
+
+header("location:managememberadd.html");
+
 ?>
