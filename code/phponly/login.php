fa196161234e63b281419814a97b44cde8746e3b
annanharry <annanharry@students.uonbi.ac.ke>
2022-06-15 19:28:34 +0300

    login yet to be tested

--- code/phponly/login.php
+++ code/phponly/login.php
@@ -1,6 +1,29 @@
 <?php
-    session_start();
     include "connect.php";
+    session_start();
+
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+        $password = mysqli_real_escape_string($conn,$_POST['username']);
+        $username = mysqli_real_escape_string($conn,$_POST['password']);
+
+        $md5password = md5($password);
+
+        $sql = "SELECT staff_id FROM staff WHERE username = '$username' AND password = '$md5password'";
+        $result = mysqli_query($conn,$sql);
+        $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+        $active = $row['active'];
+
+        $count = mysqli_num_rows($result);
+
+        if ($count == 1){
+            $_SESSION['login_user'] = $username;
+
+            header("location: ../search.php");
+        }
+    }else{
+        $error="The Username or Password is invalid";
+    }
 
     header("location:../search.php");
 ?>
