62b0b82c45224e0b526237f0f7fe6e405925515f
annanharry <annanharry@students.uonbi.ac.ke>
2022-06-27 20:15:25 +0300

    need to fix the query

--- code/guarantors.php
+++ code/guarantors.php
@@ -3,11 +3,21 @@
     session_start();
 
     //post the loan amount and type, before guarantors update is made.
-    //$member = $_SESSION['search_id'];
+    $member = $_SESSION['search_id'];
     $loan_type = $_POST['loan_type'];
-    //$loan_amount = $_POST['loanrequest'];
+    $loan_amount = $_POST['loanrequest'];
 
-    //echo $member;
-    echo $loan_type;
-    //echo $loan_amount;
+    $interestsql = "SELECT interest_rate, repayment FROM loan_type WHERE loan_type_id LIKE %$loan_type%";
+    $interestresult = $conn->query($interestsql);
+    $interestrow = $interestresult -> fetch_assoc();
+    $interest = $interestrow["interest_rate"];
+    $repayment = $interestrow["repayment"];
+
+    $new_loan_amount = $loan_amount * $interest;
+    $installments = $new_loan_amount / $repayment;
+
+    $sql = mysqli_query($conn, "INSERT INTO loans(member_id, loan_type_id, amount, installments) 
+                                    VALUES ('$member','$loan_type','$new_loan_amount','$installments')");
+
+    echo "check database";
 ?>
