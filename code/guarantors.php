ee1ff0e0b8d01da5a9de9703b1d81982ebef32e8
annanharry <annanharry@students.uonbi.ac.ke>
2022-06-28 15:50:10 +0300

    Loan upload works on to the next

--- code/guarantors.php
+++ code/guarantors.php
@@ -7,17 +7,17 @@
     $loan_type = $_POST['loan_type'];
     $loan_amount = $_POST['loanrequest'];
 
-    $interestsql = "SELECT interest_rate, repayment FROM loan_type WHERE loan_type_id LIKE %$loan_type%";
-    $interestresult = $conn->query($interestsql);
+    $interestsql = "SELECT * FROM loan_type WHERE loan_type_id = $loan_type";
+    $interestresult = mysqli_query($conn,$interestsql);
     $interestrow = $interestresult -> fetch_assoc();
     $interest = $interestrow["interest_rate"];
     $repayment = $interestrow["repayment"];
 
-    $new_loan_amount = $loan_amount * $interest;
-    $installments = $new_loan_amount / $repayment;
+    $new_loan_amount = $loan_amount + ($loan_amount * $interest);
+    $instalments = $new_loan_amount / $repayment;
 
-    $sql = mysqli_query($conn, "INSERT INTO loans(member_id, loan_type_id, amount, installments) 
-                                    VALUES ('$member','$loan_type','$new_loan_amount','$installments')");
+    $sql = mysqli_query($conn, "INSERT INTO loans(member_id, loan_type_id, amount, instalments) 
+                                    VALUES ('$member','$loan_type','$new_loan_amount','$instalments')");
 
     echo "check database";
 ?>
