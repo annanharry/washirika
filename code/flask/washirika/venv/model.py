bc3b316fe00d1fc839e39ce0c81bc7df9ca09cb5
annanharry <annanharry@students.uonbi.ac.ke>
2022-08-02 15:10:42 +0300

    flask stuff

---
+++ code/flask/washirika/venv/model.py
@@ -0,0 +1,12 @@
+import pandas as pd
+#import numpy as np
+#import joblib
+#from sklearn import datasets, linear_model
+#from sklearn.model_selection import train_test_split
+#from sklearn.preprocessing import StandardScaler
+#from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+#from sklearn.linear_model import LogisticRegression
+
+#reading csv file with pandas
+dataset=pd.read_csv('D:\Xampp\htdocs\washirika-1\code\flask\washirika\venv\cleaned1.csv')
+
